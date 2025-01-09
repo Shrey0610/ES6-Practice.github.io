5959fef18015b0e300b070001bd0a27982569c5c
Shrey Shah <119574537+Shrey0610@users.noreply.github.com>
2025-01-09 09:58:59 +0530

    added third.js with advanced topics

---
+++ third.js
@@ -0,0 +1,13 @@
+// //Passing a function call (x=>x*2) inside a function as a parameter
+// function copyandmanipulate(array,instruction){ //Higher-order Function
+//     const output=[];
+//     for(let i=0; i<array.length; i++){
+//         output.push(instruction(array[i]));
+//     }
+//     return output;
+// }
+
+// const result= copyandmanipulate([1,2,4], x => x*2); //CallBack: function multiply2(x){return x*2;} --> x=>x*2 using arrow function
+// console.log(result);
+
+
